9b6a6a031f2774b3c370ff5d1c9d36e842e398cb
Siddhesh-Codes <siddhesh.codes21@gmail.com>
2026-04-23 00:13:54 +0530

    Autoboxing and Unboxing

--- Lecture 18/code/Demo.java
+++ Lecture 18/code/Demo.java
@@ -10,6 +10,6 @@ class Sample {}
 class Sample2 {}
 
 // 1. Why one public class per file.
-//2 WHy name of that public class should be same as filename
+// 2. WHy name of that public class should be same as filename
 
 // JVM --> Demo.java. --> Demo.main()
